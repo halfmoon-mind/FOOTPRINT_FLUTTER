48a33f4fdb6b8381add2be685fc6828fe957c5c4
Yujin <jini1514@soongsil.ac.kr>
2022-08-20 18:30:02 +0900

    [Add] 로그아웃 기능 추가
    Setting 버튼을 LogOut 버튼으로 변경 및 로그아웃 기능 구현

--- FOOTPRINT_FLUTTER/lib/tabs/tabs_home.dart
+++ FOOTPRINT_FLUTTER/lib/tabs/tabs_home.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:FOOTPRINT_FLUTTER/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:FOOTPRINT_FLUTTER/models/user.dart';
 
 // import 'package:flutter/src/foundation/key.dart';
 // import 'package:flutter/src/widgets/framework.dart';
+String? UID;
 
 class HomeTab extends StatelessWidget {
+  Future setLogout() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isLogin', false);
+    prefs.setString('UID', '');
+    print('[*] 로그인 상태 : ' + prefs.getBool('isLogin').toString());
+  }
+
+  Future getUID() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    UID = prefs.getString('UID');
+  }
+
+  Future<List<USER>> getUserData(UID) => FirebaseFirestore.instance
+      .collection('user')
+      .where('uid', isEqualTo: UID)
+      .get()
+      .then((snapshot) =>
+          snapshot.docs.map((doc) => USER.fromJson(doc.data())).toList());
+
   @override
   Widget build(BuildContext context) {
+    getUID();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -41,9 +67,12 @@ class HomeTab extends StatelessWidget {
                 Icons.settings,
                 color: Palette.kToGreen[500],
               ),
-              title: Text('Setting'),
-              onTap: () {
-                Navigator.of(context).pop();
+              title: Text('LogOut'),
+              onTap: () async {
+                // Navigator.of(context).pop();
+                await FirebaseAuth.instance.signOut();
+                await setLogout();
+                Navigator.of(context).pushReplacementNamed('/login');
               },
             ),
             ListTile(
