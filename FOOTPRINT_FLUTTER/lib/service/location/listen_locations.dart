bdde6d9eef8ca37caed788a155e237fed07d5b60
White-Patrasche <5338095@gmail.com>
2022-08-22 01:44:38 +0900

    [Refactoring] 위치 데이터 수신부 함수 필요한 부분만 제작

---
+++ FOOTPRINT_FLUTTER/lib/service/location/listen_locations.dart
@@ -0,0 +1,23 @@
+import 'package:location/location.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+import 'package:FOOTPRINT_FLUTTER/service/hive/oneday.dart';
+
+class ListenLocations {
+  StreamSubscription<LocationData>? _locationSubscription;
+  LocationData? _location;
+  listenLocations() async {
+    _locationSubscription =
+        onLocationChanged(inBackground: true).handleError((dynamic err) {
+      if (err is PlatformException) {
+        print(err.code);
+      }
+      _locationSubscription?.cancel();
+      _locationSubscription = null;
+    }).listen((LocationData currentLocation) async {
+      _location = currentLocation;
+      oneDayDataService()
+          .addOneDayData(currentLocation.latitude, currentLocation.longitude);
+    });
+  }
+}
